218a048d56fd067cf0e3e86d25ddc2008f3b5a20
Michael Sturm <michael.c.sturm@gmail.com>
2024-11-14 15:51:13 -0800

    Added test cases!

--- main.py
+++ main.py
@@ -5,6 +5,50 @@ import stock_visualization
 from datetime import datetime
 
 class variables:
+    def q1(answer):
+            answer = answer.upper()
+            if len(answer) > 7 or len(answer) < 1 or answer.isalpha() == False:
+                print("Hey make sure your answer is 1-7 alphabetical characters!")
+                return 1
+            variables.listOfInputs.append(answer)
+            return 0
+    def q2(answer):
+            answer = answer.upper()
+            if len(answer) != 1 or answer.isalpha() or int(answer)>2 or int(answer)<1:
+                print("Hey make sure your answer is 1 or 2!")
+                return 1
+            variables.listOfInputs.append(answer)
+            return 0
+    def q3(answer):
+            answer = answer.upper()
+            if len(answer) != 1 or answer.isalpha() or int(answer)>4 or int(answer)<1:
+                print("Hey make sure your answer is 1 through 4!")
+                return 1
+            variables.listOfInputs.append(answer)
+            return 0
+    def q4(answer):
+            answer = answer.upper()
+            try:
+                sendData =datetime.strptime(answer, '%Y-%m-%d')
+                variables.stringStartDate = answer
+                variables.listOfInputs.append(sendData)
+                return 0
+            except:
+                print("Hey make sure your answer is formated like %Y-%m-%d'")
+                return 1
+    def q5(answer):
+            answer = answer.upper()
+            try:
+                sendData =datetime.strptime(answer, '%Y-%m-%d')
+                variables.stringEndDate= answer
+                variables.listOfInputs.append(sendData)
+                return 0
+            except:
+                print("Hey make sure your answer is formated like %Y-%m-%d'")
+                return 1
+    def sendData():
+        stock_visualization.stockMaker(variables.listOfInputs[0],variables.listOfInputs[1],variables.listOfInputs[2],
+                                       variables.listOfInputs[3],variables.listOfInputs[4], variables.stringStartDate,variables.stringEndDate)
     listofQuestions = ['Enter the stock symbol for the company you would like',
                    'Enter chart type you would like (line: 1/bar: 2)',
                    'Enter the time series function desired (TIME_SERIES_DAILY : 1, TIME_SERIES_WEEKLY : 2, TIME_SERIES_MONTHLY : 3, TIME_SERIES_MONTHLY_ADJUSTED: 4)',
@@ -21,57 +65,8 @@ class variables:
     stringEndDate =''
 
 
-# pygame.init() will initialize all 
-# imported module 
-while True:
-    print(variables.listofQuestions[0])
-    answer = input()
-    answer = answer.upper()
-    if len(answer) > 7 or len(answer) < 1 or answer.isalpha() == False:
-        print("Hey make sure your answer is 1-7 alphabetical characters!")
-        continue
-    variables.listOfInputs.append(answer)
-    break
-while True:
-    print(variables.listofQuestions[1])
-    answer = input()
-    answer = answer.upper()
-    if len(answer) != 1 or answer.isalpha() or int(answer)>2 or int(answer)<1:
-        print("Hey make sure your answer is 1 or 2!")
-        continue
-    variables.listOfInputs.append(answer)
-    break
-while True:
-    print(variables.listofQuestions[2])
-    answer = input()
-    answer = answer.upper()
-    if len(answer) != 1 or answer.isalpha() or int(answer)>4 or int(answer)<1:
-        print("Hey make sure your answer is 1 through 4!")
-        continue
-    variables.listOfInputs.append(answer)
-    break
-while True:
-    print(variables.listofQuestions[3])
-    answer = input()
-    answer = answer.upper()
-    try:
-        datetime.strptime(answer, '%Y-%m-%d')
-        variables.listOfInputs.append(answer)
-        break
-    except:
-        print("Hey make sure your answer is formated like %Y-%m-%d'")
-        continue
-while True:
-    print(variables.listofQuestions[4])
-    answer = input()
-    answer = answer.upper()
-    try:
-        datetime.strptime(answer, '%Y-%m-%d')
-        variables.listOfInputs.append(answer)
-        break
-    except:
-        print("Hey make sure your answer is formated like %Y-%m-%d'")
-        continue
+
+
     
     
 
